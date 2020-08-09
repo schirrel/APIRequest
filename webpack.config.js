debb3880be13653d41a48d0584f55685a681eb1b
Alan Schio <6757777+schirrel@users.noreply.github.com>
2020-08-08 23:37:33 -0300

    umd

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
+        library: 'index',
+        libraryTarget: 'umd',
+        libraryExport: 'default',
         filename: 'index.js',
         path: path.resolve(__dirname, '')
     }
